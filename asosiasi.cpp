c2bf440dcd9c3895831c2f7a3b62b34bd5fe02bf
keaneozzie <ozziedharma897@gmail.com>
2026-05-11 16:45:25 +0700

    define dokter class with association to pasien

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -17,4 +17,21 @@ public:
         cout << "Pasien \"" << nama << "\" tidak ada\n";
     }
 
-    
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+   
