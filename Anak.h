cd06d09f46d782febcd1c58e99881aeead9a81e7
keaneozzie <ozziedharma897@gmail.com>
2026-05-11 15:54:17 +0700

    membuat class anak

--- Anak.h
+++ Anak.h
@@ -3,3 +3,16 @@
 #include <iostream>
 #include <string>
 
+class anak {
+public:
+    string nama;
+     anak(string pNama) : nama(pNama) {
+     cout << "Anak \"" << nama << "\" ada\n";
+    }
+
+    ~anak() {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
+
+#endif
