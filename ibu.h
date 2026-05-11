799900095c64190dc3954474914d0274f50e2c9e
keaneozzie <ozziedharma897@gmail.com>
2026-05-11 16:02:36 +0700

    add constructor and destructor for ibu class

--- ibu.h
+++ ibu.h
@@ -9,4 +9,12 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+
     
