208b4cf9296da98f8177cd9b7ab9059a00089a2c
keaneozzie <ozziedharma897@gmail.com>
2026-05-11 16:43:57 +0700

    defien pasien class with association to dokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -4,3 +4,17 @@ using namespace std;
 
 class dokter;
 
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+
+    pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+
+    
